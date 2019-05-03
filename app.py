cd431c3f058ddc74979faff99f2c16b6ef6aaa0c
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-05-03 11:27:45 -0700

    change shebang line, add () to print stmt for py3 compatibility

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!flask/bin/python
+#!/usr/bin/env python
 
 # Author: Ngo Duy Khanh
 # Email: ngokhanhit@gmail.com
@@ -61,7 +61,7 @@ def create_thumbnail(image):
         return True
 
     except:
-        print traceback.format_exc()
+        print(traceback.format_exc())
         return False
 
 
